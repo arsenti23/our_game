d015460414e405a85ad4c415f39688c26ba8d0ce
arsenti23 <arsentijakovlev@gmail.com>
2022-12-26 15:59:32 +0300

    добавил файл для класса конечной заставки

---
+++ game_over.py
@@ -0,0 +1,11 @@
+# Здесь опишем класс конечной заставки
+# обязательный метод update()
+import pygame
+
+
+class Game_over(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
