22a927be208404284667bc7e81f76b22ed216e99
Vesnud <Vesnud@yandex.ru>
2023-02-27 07:04:49 +0300

    player

--- game.py
+++ game.py
@@ -2,29 +2,44 @@ import pygame
 import os
 import sys
 
-COLOR1 = (0, 0, 0)
-COLOR2 = (255, 255, 255)
-COLORR = (255, 0, 0)
-COLORG = (0, 255, 0)
-COLORB = (0, 0, 255)
+
 
 pygame.init()
-size = width, height = 800, 800
+size = width, height = 1000, 600
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("My game")
 clock = pygame.time.Clock()
 running = True
+color1 = (0, 0, 0)
+color2 = (255, 255, 255)
+color3 = (255, 0, 0)
+color4 = (0, 255, 0)
+color5 = (0, 0, 255)
 FPS = 30
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((70, 50))
+        self.image.fill(color5)
+        self.rect = self.image.get_rect()
+        self.rect.center = (0, height // 2)
+
+    def update(self):
+        pass
 
 def terminate(self):
     pygame.quit()
     sys.exit()
 
+my_sprites = pygame.sprite.Group()
+player = Player()
+my_sprites.add(player)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
-    screen.fill(COLOR1)
+    screen.fill(color1)
+    my_sprites.draw(screen)
     pygame.display.flip()
