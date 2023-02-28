229b9fbe92c99e4c9046dba937fde7e0ceb3762c
Vesnud <Vesnud@yandex.ru>
2023-02-28 06:07:48 +0300

    движение

--- game.py
+++ game.py
@@ -2,10 +2,8 @@ import pygame
 import os
 import sys
 
-
-
 pygame.init()
-size = width, height = 1000, 600
+size = width, height = 600, 700
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My game")
 clock = pygame.time.Clock()
@@ -17,29 +15,48 @@ color4 = (0, 255, 0)
 color5 = (0, 0, 255)
 FPS = 30
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((70, 50))
+        self.w, self.h = 70, 70
+        self.image = pygame.Surface((self.w, self.h))
         self.image.fill(color5)
         self.rect = self.image.get_rect()
-        self.rect.center = (0, height // 2)
+        self.x, self.y = width // 2, height - 40
+        self.rect.center = self.x, self.y
+        self.coordx = 0
 
     def update(self):
-        pass
+
+        self.coordx = 0
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                self.coordx = -1
+            if event.key == pygame.K_RIGHT:
+                self.coordx = 1
+        self.rect.x += self.coordx
+        if self.rect.right > width:
+            self.rect.right = 0 + self.w
+        elif self.rect.left < 0:
+            self.rect.left = width - self.w
 
 def terminate(self):
     pygame.quit()
     sys.exit()
 
-my_sprites = pygame.sprite.Group()
+
+all_sprites = pygame.sprite.Group()
 player = Player()
-my_sprites.add(player)
+all_sprites.add(player)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
+    all_sprites.update()
     screen.fill(color1)
-    my_sprites.draw(screen)
+    all_sprites.draw(screen)
     pygame.display.flip()
+pygame.quit()
